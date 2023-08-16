bf75b9dc539302b046073e1c9210138b9d2e6647
Gerard Kamran Kalambacal <kamrangerardkalambacal@gmail.com>
2023-08-16 17:25:45 +0800

    feat: implemented should_return_message_with_name_and_age_when_introduce_given_teacher

--- src/main/java/ooss/Teacher.java
+++ src/main/java/ooss/Teacher.java
@@ -9,4 +9,7 @@ public class Teacher extends Person {
     public Teacher(int id, String name, int age) {
         super(id, name, age);
     }
+    public String introduce() {
+        return String.format(super.introduce()+" I am a teacher.");
+    }
 }
