4e71390ce448c25ae2bdeac6e16c4221fcd50320
Gerard Kamran Kalambacal <kamrangerardkalambacal@gmail.com>
2023-08-16 17:49:04 +0800

    feat: implemented should_return_result_by_student_is_in_class_when_isIn_given_student

--- src/main/java/ooss/Student.java
+++ src/main/java/ooss/Student.java
@@ -1,13 +1,20 @@
 package ooss;
 
 public class Student extends Person {
-
+    private Klass klass;
     public Student(int id, String name, int age) {
         super(id, name, age);
     }
+    public void join(Klass klass) {
+        this.klass = klass;
+    }
 
+    public boolean isIn(Klass klass) {
+        return this.klass != null && this.klass.equals(klass);
+    }
     @Override
     public String introduce() {
         return String.format(super.introduce()+" I am a student.");
     }
+
 }
