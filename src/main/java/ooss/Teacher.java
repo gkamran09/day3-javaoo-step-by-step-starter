0658fa1bad0df7b467ef6fb9e2e31bb212f2c4fa
Gerard Kamran Kalambacal <kamrangerardkalambacal@gmail.com>
2023-08-16 17:52:34 +0800

    feat: implemented should_return_result_by_is_belong_to_when_belongsTo_given_teacher_teaches_in_the_class

--- src/main/java/ooss/Teacher.java
+++ src/main/java/ooss/Teacher.java
@@ -6,8 +6,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Teacher extends Person {
+    private Klass klass;
     public Teacher(int id, String name, int age) {
         super(id, name, age);
+    }
+    public void assignTo(Klass klass) {
+        this.klass = klass;
+    }
+
+    public boolean belongsTo(Klass klass) {
+        return this.klass != null && this.klass.equals(klass);
     }
     public String introduce() {
         return String.format(super.introduce()+" I am a teacher.");
